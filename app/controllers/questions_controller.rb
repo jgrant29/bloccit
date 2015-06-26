8759ff4c743576ecec5d001bed2070b0c48a64aa
Bobbilee Hartman <bobbilee_hartman@intuit.com>
2015-06-25 21:19:18 -0700

    cleaned

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,16 +19,16 @@ class QuestionsController < ApplicationController
     else
       flash[:error] = "There was an error saving the question.  Please try again."
       render :new
-    end 
+    end
   end
-  
+
   def edit
      @question = Question.find(params[:id])
   end
 
   def update
      @question = Question.find(params[:id])
-     if @question.update_attributes(params.require(:question).permit(:title, :body))
+     if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
        flash[:notice] = "Question was updated."
        redirect_to @question
      else
@@ -41,27 +41,4 @@ class QuestionsController < ApplicationController
      @question = Question.destroy(params[:id])
      redirect_to @question
    end
-
-
-   def d_update
-     @question = Question.find(params[:id])
-     if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
-       flash[:notice] = "Question was updated."
-       redirect_to @question
-     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :edit
-     end
-   end
-
-   def resolved
-    @question = Question.update(params[:id])
-    if @question.update_attributes(params[:resolved])
-      flash[:notice] = "Marked was Resolved."
-      redirect_to @question
-    else
-      flash[:error] = "That didn't work"
-      render :edit
-    end
-  end
 end
