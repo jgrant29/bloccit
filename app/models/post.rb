b464515fe14401b1b4ddeff8d8bdbf75f59c6ca9
Justin Grant <jmgrant702@gmail.com>
2015-06-27 13:42:17 -0600

    associations User Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
+
+  default_scope { order('created_at DESC') }
   validates_uniqueness_of :title
   validates_uniqueness_of :body
 end
