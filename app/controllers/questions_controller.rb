f3606e69f6d4da49e1c8e86d516e304fb041417c
Justin Grant <jmgrant702@gmail.com>
2015-06-29 12:12:41 -0600

    Associations assignment — scopes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,15 +2,12 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
-
   def show
     @question = Question.find(params[:id])
   end
-
   def new
     @question = Question.new
   end
-
   def create
     @question = Question.new(params.require(:question).permit(:title, :body))
     if @question.save
@@ -21,11 +18,9 @@ class QuestionsController < ApplicationController
       render :new
     end
   end
-
   def edit
      @question = Question.find(params[:id])
   end
-
   def update
      @question = Question.find(params[:id])
      if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
@@ -36,7 +31,6 @@ class QuestionsController < ApplicationController
        render :edit
      end
    end
-
    def destroy
      @question = Question.destroy(params[:id])
      redirect_to @question
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+default_scope { order('created_at DESC') }
+scope :ordered_by_title, -> { order('title DESC') }
+scope :ordered_by_reverse_order, -> { order('created_at ASC') }
+
   validates_uniqueness_of :title
   validates_uniqueness_of :body
 end
