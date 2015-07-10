000f7f4eb65ad133d1465127897bffa1c2aaab3f
Justin Grant <jmgrant702@gmail.com>
2015-07-09 19:49:43 -0600

    interlude fundamentals

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = Post.new(post.params)
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved"
@@ -37,11 +37,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     authorize @post
 
-      if @post.update_attributes(params.require(:post).permit(:title, :body))
+      if @post = Post.new(post.params)
        flash[:notice] = "Post was updated."
        redirect_to [@topic, @post]
      else
        scope.where(user: user)
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
